fcf17e53d8e5cfbf4b707c6bbb81be036a26d3e8
ryotogashi <ryo.togashi.ca@gmail.com>
2020-03-10 14:20:40 -0700

    add animation

---
+++ singlepage-layout/scroll.js
@@ -0,0 +1,34 @@
+const option = {
+  delay: 400,
+  duration: 2000,
+  easing: 'ease-in',
+  opacity: 0
+};
+const option2 = {
+  ...option,
+  rotate: {
+    x: 0,
+    y: 90,
+    z: 0
+  }
+};
+const option3 = {
+  ...option,
+  rotate: {
+    x: 0,
+    y: 0,
+    z: 90
+  }
+};
+const option4 = {
+  ...option,
+  rotate: {
+    x: 90,
+    y: 90,
+    z: 90
+  }
+};
+ScrollReveal().reveal('.about', option);
+ScrollReveal().reveal('.works', option2);
+ScrollReveal().reveal('.skills', option3);
+ScrollReveal().reveal('.contact', option4);
---
+++ singlepage-layout/type.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.site-title', {
+  strings: ['HI, MY NAME IS Ryo Togashi'],
+  typeSpeed: 200,
+  backSpeed: 0,
+  fadeOut: true,
+  loop: false,
+  showCursor: false
+});
